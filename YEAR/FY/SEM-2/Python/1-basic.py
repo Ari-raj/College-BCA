a5d0efa050c25f438909337e0a00f494b5de43b9
Ari-raj <111754245+Ari-raj@users.noreply.github.com>
2023-02-09 20:03:32 +0530

    Create 1-basic.py

---
+++ YEAR/FY/SEM-2/Python/1-basic.py
@@ -0,0 +1,39 @@
+n=input("name:")
+sur=input("enter your surname: ")
+cit=input('city name where you live:')
+col=input("enter your college name: ")
+age=int(input("enter your age:"))
+st=input('enter your stream name:')
+yr=input('enter what year you are studing:')
+print('\n')
+print('bio data:')
+print('\n')
+print("full name:",n,sur)
+print("age:",age,'\tyour city:',cit)
+print("college name:",col)
+print('studing in :',st)
+print('year:',yr)
+
+'''
+output:
+
+name:Ariba
+enter your surname: Rajbhara
+city name where you live:surat
+enter your college name: sdjic
+enter your age:18
+enter your stream name:bca
+enter what year you are studing:fy
+
+
+bio data:
+
+
+full name: Ariba Rajbhara
+age: 18 	your city: surat
+college name: sdjic
+studing in : bca
+year: fy
+
+
+>>> '''
