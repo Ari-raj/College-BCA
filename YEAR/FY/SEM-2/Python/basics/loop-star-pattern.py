0acbd18fcec626d7ba8af7b2cb4814b0cabb189f
Ari-raj <111754245+Ari-raj@users.noreply.github.com>
2023-03-05 22:08:55 +0530

    Create loop-star-pattern.py

---
+++ YEAR/FY/SEM-2/Python/basics/loop-star-pattern.py
@@ -0,0 +1,20 @@
+n=int(input('enter n:'))
+for i in range(0,n):
+      for j in range(0,i+1):
+          print('*',end='')
+      print('\r')
+
+
+'''
+
+OUTPUT:1
+
+enter n:6
+*
+**
+***
+****
+*****
+******
+
+'''
