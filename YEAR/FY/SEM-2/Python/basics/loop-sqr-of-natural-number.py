97cca8b32b51563f2485b92370d8d345676fee9b
Ari-raj <111754245+Ari-raj@users.noreply.github.com>
2023-03-05 22:06:10 +0530

    Create loop-sqr-of-natural-number.py

---
+++ YEAR/FY/SEM-2/Python/basics/loop-sqr-of-natural-number.py
@@ -0,0 +1,28 @@
+n=int(input('enter a number:'))
+ans=0
+for x in range(1,n+1):
+    ans=n*x
+    print(n,'*',x,'=',ans)
+    '''
+print(ans)'''
+
+''' output:1
+enter a number:10
+10 * 1 = 10
+10 * 2 = 20
+10 * 3 = 30
+10 * 4 = 40
+10 * 5 = 50
+10 * 6 = 60
+10 * 7 = 70
+10 * 8 = 80
+10 * 9 = 90
+10 * 10 = 100
+
+output : 2
+
+enter a number:2
+2 * 1 = 2
+2 * 2 = 4
+
+'''
