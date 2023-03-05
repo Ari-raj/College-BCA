f5723fc6aefd66230312e6956d26639a98042256
Ari-raj <111754245+Ari-raj@users.noreply.github.com>
2023-03-05 22:12:38 +0530

    Create loop-rev-pattern-j.py

---
+++ YEAR/FY/SEM-2/Python/basics/loop-rev-pattern-j.py
@@ -0,0 +1,18 @@
+n=int(input('enter n:'))
+for i in range(n,0,-1):
+    for j in range(i,0,-1):
+        print(j,end='')
+    print('\r')
+
+
+'''
+
+output : 1
+
+enter n:5
+54321
+4321
+321
+21
+1
+ '''
