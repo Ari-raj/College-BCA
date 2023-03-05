b580fe7e823de570ef702389de0bc7f87341ab2b
Ari-raj <111754245+Ari-raj@users.noreply.github.com>
2023-03-05 22:21:29 +0530

    Create find-max-two-no.py

---
+++ YEAR/FY/SEM-2/Python/Mix Programs/find-max-two-no.py
@@ -0,0 +1,23 @@
+n1=int(input('enter n1:'))
+n2=int(input('enter n2:'))
+if n1<n2:
+    print('',n1,'is smaller then',n2)
+else:
+    print('',n2,'is smaller then',n1)
+
+'''
+
+output:1
+
+enter n1:34
+enter n2:322
+ 34 is smaller then  322
+
+
+output :2
+
+enter n1:22222222
+enter n2:89
+ 89 is smaller then 22222222
+
+ '''
