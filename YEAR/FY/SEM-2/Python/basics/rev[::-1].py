c3bcfd377013fd9ff0cec99b88649770166cbe3c
Ari-raj <111754245+Ari-raj@users.noreply.github.com>
2023-03-05 22:16:52 +0530

    Create rev[::-1].py

---
+++ YEAR/FY/SEM-2/Python/basics/rev[::-1].py
@@ -0,0 +1,17 @@
+def rev(str):
+    str2=str[::-1]
+    print(str2)
+str=input("enter the char or no to rev:")
+rev(str)
+
+
+'''
+output:1
+
+enter the char or no to rev:1245
+5421
+
+output:2
+
+enter the char or no to rev:anika
+akina '''
