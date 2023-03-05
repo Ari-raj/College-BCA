5818040586bd0f404eba7b7495d31e14bb6239a1
Ari-raj <111754245+Ari-raj@users.noreply.github.com>
2023-03-05 21:00:28 +0530

    Update Union-pov.c

--- YEAR/FY/SEM-2/C/Union and Structure/Union-pov.c
+++ YEAR/FY/SEM-2/C/Union and Structure/Union-pov.c
@@ -1,1 +1,20 @@
-
+#include<conio.h>
+#include<stdio.h>
+#include<string.h>
+union students
+{
+char nm[14];
+int rn;
+};
+void main()
+{
+union students s[10];
+clrscr();
+printf("enter name and rollno :");
+scanf("%s%d",&s[1].nm,&s[1].rn);
+printf("nmae:%s\trn:%d\n",s[1].nm,s[1].rn);
+printf("\nenter rn and name:");
+scanf("%d%s",&s[2].rn,&s[2].nm);
+printf("rn:%d\tname:%s\n",s[2].rn,s[2].nm);
+getch();
+}
