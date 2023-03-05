a5c19f7f0ec362c10d27f18126c29dc0af177af1
Ari-raj <111754245+Ari-raj@users.noreply.github.com>
2023-03-05 22:14:17 +0530

    Create def-palindrom.py

---
+++ YEAR/FY/SEM-2/Python/Def/def-palindrom.py
@@ -0,0 +1,16 @@
+str=input('enter string to check:')
+str2=str[::-1]
+if str2 == str:
+    print('palindrom')
+else:
+    print('not palindrom')
+
+
+'''
+output:1
+enter string to check:mom
+palindrom
+
+output:2
+enter string to check:6789
+not palindrom'''
