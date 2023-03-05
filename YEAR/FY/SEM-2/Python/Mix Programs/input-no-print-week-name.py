61e1b44ba298bdf19b24262bb03e8d21fb0f18a2
Ari-raj <111754245+Ari-raj@users.noreply.github.com>
2023-03-05 22:19:33 +0530

    Create input-no-print-week-name.py

---
+++ YEAR/FY/SEM-2/Python/Mix Programs/input-no-print-week-name.py
@@ -0,0 +1,36 @@
+week=int(input("enter week number:"))
+if week==1:
+    print('monday')
+elif week==2:
+    print('tuesday')
+elif week==3:
+    print('wednesday')
+elif week==4:
+    print('thursday')
+elif week==5:
+    print('friday')
+elif week==6:
+    print('saturday')
+elif week==7:
+    print('sunday')
+else:
+    print('''invalid number!!
+hint:
+monday=1...7=sunday''')
+
+
+'''
+
+output:1
+
+enter week number:4
+thursday
+
+output:2
+
+enter week number:56
+invalid number!!
+hint:
+monday=1...7=sunday
+
+'''
