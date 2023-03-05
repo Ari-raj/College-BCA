e48776a46f2e18aae82704a7f73c2a02f10563d9
Ari-raj <111754245+Ari-raj@users.noreply.github.com>
2023-03-05 22:20:05 +0530

    Create input-no-print-month-name.py

---
+++ YEAR/FY/SEM-2/Python/Mix Programs/input-no-print-month-name.py
@@ -0,0 +1,39 @@
+month=int(input("enter month number:"))
+if month==1:
+    print("january")
+elif month==2:
+    print("febuary")
+elif month==3:
+    print("march")
+elif month==4:
+    print("april")
+elif month==5:
+    print("may")
+elif month==6:
+    print("june")
+elif month==7:
+    print("july")
+elif month==8:
+    print("augest")
+elif month==9:
+    print("september")
+elif month==10:
+    print("october")
+elif month==11:
+    print("november")
+elif month==12:
+    print("december")
+
+    '''
+
+output:1
+
+enter month number:12
+december
+
+output:2
+
+enter month number:4
+april
+
+'''
